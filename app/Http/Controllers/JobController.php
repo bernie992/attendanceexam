d4118f17031e2144b394e466de5a939dda512188
bernie992 <torresbernie992@gmail.com>
2022-06-06 16:45:46 +0800

    second update june 06

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function jobList()
+    {
+        return view('job/joblist');
+    }
+    // job view
+    public function jobView()
+    {
+        return view('job/jobview');
+    }
+}
